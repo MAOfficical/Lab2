336128d31af14c587d0dfe4b637a88b280efea0a
MAOfficical <marcuslafrance123@gmail.com>
2024-10-30 20:36:38 -0400

    test2

--- Assignment.py
+++ Assignment.py
@@ -1,6 +1,19 @@
 import streamlit as st
+import requests
 
 
-st.title("Stream App")
-
-st.subheader("Welcome to my Streamlit")
+def get_weather_data(lat, lon):
+    # Replace 'YOUR_API_KEY' with your actual API key
+    api_key = 'YOUR_API_KEY'
+    weather_url = f"http://api.openweathermap.org/data/2.5/weather?lat={lat}&lon={lon}&appid={api_key}&units=metric"
+    air_quality_url = f"http://api.openweathermap.org/data/2.5/air_pollution?lat={lat}&lon={lon}&appid={api_key}"
+    
+    weather_response = requests.get(weather_url).json()
+    air_quality_response = requests.get(air_quality_url).json()
+    
+    # Extracting relevant data
+    temperature = weather_response.get("main", {}).get("temp")
+    humidity = weather_response.get("main", {}).get("humidity")
+    aqi = air_quality_response.get("list", [{}])[0].get("main", {}).get("aqi")
+    
+    return temperature, humidity, aqi
